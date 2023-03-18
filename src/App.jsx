186912aec917dbaf2e081cc3473ed149b74fd972
sarathChandran94 <sarathcs3c@gmail.com>
2023-03-19 00:37:39 +0530

    error correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import "./App.css";
-import Home from "./components/home/Home";
+import { useState } from "react"
+import "./App.css"
+import Home from "./components/home/Home"
 
 function App() {
+  console.log("1App")
   return (
     <div className="App">
       <Home />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
