19ad50b3b120e67d90d8f3b9f49b579e28ac4690
sarathChandran94 <sarathcs3c@gmail.com>
2023-04-19 21:01:51 +0530

    added a function when timer is finished

--- src/components/date/DateAndTime.jsx
+++ src/components/date/DateAndTime.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react"
 import Cal from "../../assets/calendar.svg"
 import Hourglass from "../../assets/hourglass.svg"
 import FlipCountdown from "@rumess/react-flip-countdown"
+import { useState } from "react"
 
 const DateAndTime = () => {
   const dateFormat = new Intl.DateTimeFormat("en-in", { dateStyle: "full" })
@@ -11,14 +12,15 @@ const DateAndTime = () => {
   let date = dateFormat.format(saveTheDate)
 
   // for FlipCountdown module
+  const [isTimeUp, setIsTimeUp] = useState(false)
   const timeUpFn = () => {
-    alert("time's Up!!")
+    setIsTimeUp(true)
   }
-
   // fade animations using IntersectionObserver
 
   const dateRef = useRef()
   const timerRef = useRef()
+  // const canvasRef = useRef()
   const myoptions = {
     threshold: 1
   }
@@ -35,8 +37,135 @@ const DateAndTime = () => {
   }, myoptions)
 
   useEffect(() => {
+    // console.log(canvasRef.current)
     revealOnScroll.observe(dateRef.current)
     revealOnScroll.observe(timerRef.current)
+    //-----------Var Inits--------------
+    let canvas = document.getElementById("canvas")
+    // let canvas = canvasRef.current
+    let ctx = canvas.getContext("2d")
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    let cx = ctx.canvas.width / 2
+    let cy = ctx.canvas.height / 2
+
+    let confetti = []
+    const confettiCount = 300
+    const gravity = 0.5
+    const terminalVelocity = 5
+    const drag = 0.075
+    const colors = [
+      { front: "red", back: "darkred" },
+      { front: "green", back: "darkgreen" },
+      { front: "blue", back: "darkblue" },
+      { front: "yellow", back: "darkyellow" },
+      { front: "orange", back: "darkorange" },
+      { front: "pink", back: "darkpink" },
+      { front: "purple", back: "darkpurple" },
+      { front: "turquoise", back: "darkturquoise" }
+    ]
+
+    //-----------Functions--------------
+    const resizeCanvas = () => {
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight
+      cx = ctx.canvas.width / 2
+      cy = ctx.canvas.height / 2
+    }
+
+    const randomRange = (min, max) => Math.random() * (max - min) + min
+
+    const initConfetti = () => {
+      for (let i = 0; i < confettiCount; i++) {
+        confetti.push({
+          color: colors[Math.floor(randomRange(0, colors.length))],
+          dimensions: {
+            x: randomRange(10, 20),
+            y: randomRange(10, 30)
+          },
+
+          position: {
+            x: randomRange(0, canvas.width),
+            y: canvas.height - 1
+          },
+
+          rotation: randomRange(0, 2 * Math.PI),
+          scale: {
+            x: 1,
+            y: 1
+          },
+
+          velocity: {
+            x: randomRange(-25, 25),
+            y: randomRange(0, -50)
+          }
+        })
+      }
+    }
+
+    //---------Render-----------
+    const render = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+      confetti.forEach((confetto, index) => {
+        let width = confetto.dimensions.x * confetto.scale.x
+        let height = confetto.dimensions.y * confetto.scale.y
+
+        // Move canvas to position and rotate
+        ctx.translate(confetto.position.x, confetto.position.y)
+        ctx.rotate(confetto.rotation)
+
+        // Apply forces to velocity
+        confetto.velocity.x -= confetto.velocity.x * drag
+        confetto.velocity.y = Math.min(
+          confetto.velocity.y + gravity,
+          terminalVelocity
+        )
+        confetto.velocity.x +=
+          Math.random() > 0.5 ? Math.random() : -Math.random()
+
+        // Set position
+        confetto.position.x += confetto.velocity.x
+        confetto.position.y += confetto.velocity.y
+
+        // Delete confetti when out of frame
+        if (confetto.position.y >= canvas.height) confetti.splice(index, 1)
+
+        // Loop confetto x position
+        if (confetto.position.x > canvas.width) confetto.position.x = 0
+        if (confetto.position.x < 0) confetto.position.x = canvas.width
+
+        // Spin confetto by scaling y
+        confetto.scale.y = Math.cos(confetto.position.y * 0.1)
+        ctx.fillStyle =
+          confetto.scale.y > 0 ? confetto.color.front : confetto.color.back
+
+        // Draw confetti
+        ctx.fillRect(-width / 2, -height / 2, width, height)
+
+        // Reset transform matrix
+        ctx.setTransform(1, 0, 0, 1, 0, 0)
+      })
+
+      // Fire off another round of confetti
+      if (confetti.length <= 10) initConfetti()
+
+      window.requestAnimationFrame(render)
+    }
+
+    //---------Execution--------
+    initConfetti()
+    render()
+
+    //----------Resize----------
+    window.addEventListener("resize", function () {
+      resizeCanvas()
+    })
+
+    //------------Click------------
+    window.addEventListener("click", function () {
+      initConfetti()
+    })
   })
 
   // for BACKUP TIMER
@@ -83,17 +212,31 @@ const DateAndTime = () => {
           </div>
         </div>
         <div ref={timerRef} className="countdown rotate-in">
-          <div className="icon">
-            <img src={Hourglass} alt="hourglassIcon" />
-          </div>
-          <div className="timer">
-            <FlipCountdown
-              size="small"
-              hideYear
-              hideMonth
-              endAt={"2023-04-30 10:00:00"}
-              // onTimeUp={timeUpFn}
-            />
+          {!isTimeUp ? (
+            <>
+              <div className="icon">
+                <img src={Hourglass} alt="hourglassIcon" />
+              </div>
+              <div className="timer">
+                <FlipCountdown
+                  size="small"
+                  hideYear
+                  hideMonth
+                  // endAt={"2023-04-30 10:00:00"}
+                  endAt={"2023-04-19 21:15:00"}
+                  onTimeUp={timeUpFn}
+                />
+              </div>
+            </>
+          ) : (
+            <h3 className="congrats">CONGRATULATIONS !!!</h3>
+          )}
+          <div>
+            <canvas
+              hidden={isTimeUp ? false : true}
+              className="confetti"
+              id="canvas"
+            ></canvas>
           </div>
         </div>
       </div>
